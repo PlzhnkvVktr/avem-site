89bcf668396286625efd7b141c11e1200c5d0855
Плужников Виктор Иванович <pluzhnikov.viktor@avem.ru>
2024-06-11 16:17:51 +0300

    ref

--- src/pages/EquipmentSelection/EquipmentSelection.tsx
+++ src/pages/EquipmentSelection/EquipmentSelection.tsx
@@ -1,6 +1,8 @@
 import s from "./EquipmentSelection.module.css"
 import {ICategory} from "../../models/ICategory";
 import React, {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
+import {fetchProductsBySubcategory} from "../../store/reducers/ActionCreators";
 
 type Props = {
     categories: ICategory[]
@@ -8,15 +10,20 @@ type Props = {
 
 export const EquipmentSelection: React.FC<Props> = ({categories}) => {
 
+    const dispatch = useAppDispatch()
+    const {products, isLoadingProduct, errorProduct} = useAppSelector(state => state.productReducer)
     const stage1 = useState("")
     const stage2 = useState("")
-
-
+    const stage3 = useState("")
 
     useEffect(() => {
-        console.log(stage1[0])
-    }, []);
+        dispatch(fetchProductsBySubcategory(stage2[0]))
+    }, [stage2[0]])
 
+    useEffect(() => {
+        stage2[1]("")
+        stage3[1]("")
+    }, [stage1[0]])
 
     return (
         <main>
@@ -32,8 +39,14 @@ export const EquipmentSelection: React.FC<Props> = ({categories}) => {
                     )}
                 </form>
             </div>
-            {stage1[0] && <StageItem name={"Тип продукции 2"} array={categories[Number(stage1[0])].subcategories}/>}
 
+            {/*<StageItem name={"Тип продукции 1"} array={categories} stage={stage1} />*/}
+            {(stage1[0] && categories[Number(stage1[0])].subcategories.length > 0) &&
+                <StageItem name={"Тип продукции 2"} array={categories[Number(stage1[0])].subcategories} stage={stage2} />
+            }
+            {(stage2[0] && products.length > 0) &&
+                <StageItem name={"Продукция"} array={products} stage={stage3} />
+            }
         </main>
     )
 }
@@ -41,19 +54,23 @@ export const EquipmentSelection: React.FC<Props> = ({categories}) => {
 type Props1 = {
     array: any[]
     name: string
+    stage: [string, React.Dispatch<React.SetStateAction<string>>]
 }
 
-export const StageItem: React.FC<Props1> = ({array, name}) => {
-
+export const StageItem: React.FC<Props1> = (props) => {
 
     return (
         <div className={s.questionItem}>
-            <h3>{name}</h3>
+            <h3>{props.name}</h3>
             <form>
-                {array.map((item, key) =>
+                {props.array.map((item, key) =>
                     <label key={key}>
-                        <input type="radio" name="radio" value={item.name}
-                               />
+                        <input
+                            type="radio"
+                            name="radio"
+                            value={item.name}
+                            onClick={e => props.stage[1](e.currentTarget.value)}
+                        />
                         <span>{item.name}</span>
                     </label>
                 )}
